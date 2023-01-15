120eca48a11e1080d7485c017902dcd11c4d24f2
Preciado <aarongarpre@gmail.com>
2023-01-15 20:19:37 +0100

    Enabled multisampling

--- src/private/Engine/Core.cpp
+++ src/private/Engine/Core.cpp
@@ -16,10 +16,17 @@ void Core::Init() {
 
 	scDesc.BufferCount = 1; // We only want to create the backbuffer
 	scDesc.Windowed = TRUE;
-	scDesc.SampleDesc.Count = 4;
-	scDesc.BufferDesc.Format = DXGI_FORMAT_B8G8R8A8_UNORM;
+	scDesc.SampleDesc.Count = 8;
+	scDesc.BufferDesc.Format = DXGI_FORMAT_R10G10B10A2_UNORM;
 	scDesc.OutputWindow = this->hwnd;
 	scDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	scDesc.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+
+	D3D_FEATURE_LEVEL featureLevel[] = {
+		D3D_FEATURE_LEVEL_10_0,
+		D3D_FEATURE_LEVEL_10_1,
+		D3D_FEATURE_LEVEL_11_0,
+	};
 
 	/* Creation of our Device, Inmediate context and Swap chain */
 	HRESULT hr = D3D11CreateDeviceAndSwapChain(
@@ -27,7 +34,7 @@ void Core::Init() {
 		D3D_DRIVER_TYPE_HARDWARE,
 		NULL,
 		NULL,
-		nullptr,
+		NULL,
 		NULL,
 		D3D11_SDK_VERSION,
 		&scDesc,
@@ -79,7 +86,7 @@ void Core::Init() {
 	depthTexDesc.BindFlags = D3D11_BIND_DEPTH_STENCIL;
 	depthTexDesc.Height = this->height;
 	depthTexDesc.Width = this->width;
-	depthTexDesc.SampleDesc.Count = 4;
+	depthTexDesc.SampleDesc.Count = 8;
 
 	this->dev->CreateTexture2D(&depthTexDesc, nullptr, depthTex.GetAddressOf()); // Creation of our depth buffer texture
 
@@ -93,6 +100,18 @@ void Core::Init() {
 	this->dev->CreateDepthStencilView(depthTex.Get(), &dsvDesc, this->depthBuffer.GetAddressOf()); // Our depth stencil view creation
 	depthTex->Release();
 
+	D3D11_RASTERIZER_DESC rDesc = { };
+	ZeroMemory(&rDesc, sizeof(D3D11_RASTERIZER_DESC));
+
+	rDesc.CullMode = D3D11_CULL_BACK;
+	rDesc.AntialiasedLineEnable = TRUE;
+	rDesc.FillMode = D3D11_FILL_SOLID;
+	rDesc.MultisampleEnable = TRUE;
+
+	ComPtr<ID3D11RasterizerState> rasterizer;
+	this->dev->CreateRasterizerState(&rDesc, rasterizer.GetAddressOf());
+	
+	this->con->RSSetState(rasterizer.Get());
 	this->con->RSSetViewports(1, &viewport); // Set our viewport as actual viewport
 	this->con->OMSetRenderTargets(1, this->backBuffer.GetAddressOf(), this->depthBuffer.Get()); // Set out backbuffer and depthbuffer as render targets
 
